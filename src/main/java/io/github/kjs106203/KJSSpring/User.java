0f9b20d2493346b264fccac59a876a008ffff611
kjs106203 <kjs106203@naver.com>
2021-02-10 21:30:17 +0900

    init: initial commit

---
+++ src/main/java/io/github/kjs106203/KJSSpring/User.java
@@ -0,0 +1,11 @@
+package io.github.kjs106203.KJSSpring;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private int age;
+    private boolean sex;
+    private String address;
+}
